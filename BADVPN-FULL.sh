474bb088111ddcc9ee912a2102ea72176d3c9bac
M2SPRO4 <djmarciopgm2018@gmail.com>
2023-01-08 18:45:49 -0300

    first commit

---
+++ BADVPN-FULL.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while true
+do
+ulimit -c unlimited
+ulimit -d unlimited
+ulimit -e unlimited
+ulimit -f unlimited
+ulimit -i unlimited
+ulimit -l unlimited
+ulimit -m unlimited
+ulimit -n 999999
+ulimit -q unlimited
+ulimit -r unlimited
+ulimit -s unlimited
+ulimit -t unlimited
+ulimit -u unlimited
+ulimit -v unlimited
+ulimit -x unlimited
+/bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10000
+sleep 1
+done
